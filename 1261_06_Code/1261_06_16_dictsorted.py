7f6606e1f7841363f3f76d125fbdfef3a856f194
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-08-17 12:18:59 +0800

    adding doctest and __new__

--- 1261_06_Code/1261_06_16_dictsorted.py
+++ 1261_06_Code/1261_06_16_dictsorted.py
@@ -1,3 +1,16 @@
+'''
+>>> ds = DictSorted()
+>>> d={}
+>>> ds['a']=1
+>>> ds['c']=3
+>>> ds['b']=2
+>>> for k,v in ds.items():        print(k,v)
+a 1
+c 3
+b 2
+'''
+
+
 from collections import KeysView, ItemsView, ValuesView
 class DictSorted(dict):
     def __new__(*args, **kwargs):
@@ -28,3 +41,9 @@ class DictSorted(dict):
     def __iter__(self):
         '''for x in self syntax'''
         return self.ordered_keys.__iter__()
+
+if __name__ == "__main__":
+
+       
+    import doctest
+    doctest.testmod()         
